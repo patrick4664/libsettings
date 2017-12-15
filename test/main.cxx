adf3b78149e4136482fb99f53d45af78056989ff
patrickf2000 <patrick_dev2000@outlook.com>
2017-12-15 17:41:13 -0500

    added test file

--- test/main.cxx
+++ test/main.cxx
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <settings.hh>
 
-int main(int argc, char **argv) {
-	std::cout << "Hello!" << std::endl;
+int main() {
+    Settings::registerApp("settings.test");
+    Settings::initPaths();
+
+    Settings::writeSetting("window/winX","600");
+    Settings::writeSetting("window/winY","500");
+    Settings::writeSetting("search/terms/history","a term");
+    Settings::writeSetting("base","x2");
+    Settings::writeSetting("base/attr","attr1","attrValue","");
+
+    QString defaultSetting = Settings::getSetting("no/exist","Yahoo!");
+    QString winX = Settings::getSetting("window/winX","300");
+    QString winY = Settings::getSetting("window/winY","200");
+    QString history = Settings::getSetting("search/terms/history","term");
+    QString base = Settings::getSetting("base","x");
+    QString attr = Settings::getAttributeSetting("base/attr","attr1","aValue");
+
+    std::cout << defaultSetting.toStdString() << std::endl;
+    std::cout << winX.toStdString() << std::endl;
+    std::cout << winY.toStdString() << std::endl;
+    std::cout << history.toStdString() << std::endl;
+    std::cout << base.toStdString() << std::endl;
+    std::cout << attr.toStdString() << std::endl;
+
 	return 0;
 }
