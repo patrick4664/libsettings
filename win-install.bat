75f9d5f977e12371e028477954f81c0fb385344c
patrickf2000 <patrick_dev2000@outlook.com>
2018-01-20 08:47:05 -0500

    updated windows install script

--- win-install.bat
+++ win-install.bat
@@ -1,18 +1,18 @@
 @echo off
 if not exist build echo Please build the library first
-if not exist C:\KDE md C:\KDE
-if not exist C:\KDE\bin md C:\KDE\bin
-if not exist C:\KDE\lib md C:\KDE\lib
-if not exist C:\KDE\include md C:\KDE\include
-if not exist C:\KDE\include\cpplib md C:\KDE\include\cpplib
+if not exist C:\Unix md C:\Unix
+if not exist C:\Unix\bin md C:\Unix\bin
+if not exist C:\Unix\lib md C:\Unix\lib
+if not exist C:\Unix\include md C:\Unix\include
+if not exist C:\Unix\include\cpplib md C:\Unix\include\cpplib
 
 cd build\lib
-copy libsettings.dll C:\KDE\bin
-copy libsettings.dll.a C:\KDE\lib
+copy libsettings.dll C:\Unix\bin
+copy libsettings.dll.a C:\Unix\lib
 
 cd ../..
 cd lib
-copy settings.hh C:\KDE\include\cpplib
-copy tinyxml2.h C:\KDE\include\cpplib
+copy settings.hh C:\Unix\include\cpplib
+copy tinyxml2.h C:\Unix\include\cpplib
 
 echo Done
